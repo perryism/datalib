16f437db113b084839511ba3a75be9f3acc2bd43
Perry <perryism@gmail.com>
2020-05-14 10:35:07 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    readme = file.read()
+
+with open('requirements.txt') as file:
+    install_requires = file.readlines()
+
+setup(
+    name='datalib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    url='',
+    license='LICENSE.txt',
+    description='',
+    long_description=readme,
+    author='Perry',
+    author_email='perryism@gmail.com'
+)
