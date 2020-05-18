bed79d333fa8ed27d9fa5b0431c4ebac3d117885
Perry <perryism@gmail.com>
2020-05-18 11:48:58 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as file:
 
 setup(
     name='datalib',
-    version='0.2.1',
+    version='0.3.0',
     packages=find_packages(),
     install_requires=install_requires,
     url='',
